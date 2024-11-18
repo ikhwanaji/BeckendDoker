8235b6fe8c76cd8599e497ef8bdb1cec94a960b8
ikhwanaji <118282010+ikhwanaji@users.noreply.github.com>
2024-11-18 14:57:42 +0700

    BeckendDoker

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const { testConnection } = require("./Database/db.js");
+
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(process.env.APP_PORT, async () => {
+  await testConnection();
+  console.log(`Running at http://localhost:${process.env.APP_PORT}`);
+});
